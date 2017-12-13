d024eb98d4c81750e87637e0e6e0a089cb5ea81b
jjandoc <jjandoc@bluestatedigital.com>
2017-12-12 23:46:15 -0800

    Day 10 part 2

--- 10.js
+++ 10.js
@@ -23,4 +23,29 @@ function getResult(list, input) {
   return state[0] * state[1];
 }
 
-getResult(initialState, myInput);
+function getKnotHash(list, input) {
+  let state = list.slice();
+  let pos = 0;
+  let skipSize = 0;
+  const lengths = input.split('').map((d) => d.charCodeAt())
+      .concat([17, 31, 73, 47, 23]);
+  for (let i = 0; i < 64; i++) {
+    lengths.forEach((item, i) => {
+      state = updateState(state, pos, item);
+      pos = (pos + item + skipSize) % state.length;
+      skipSize++;
+    });
+  }
+  return state.reduce((hash, item, i) => {
+    if (i % (16) === 0 && i !== 0) {
+      hash.push([]);
+    }
+    hash[hash.length - 1].push(item);
+    return hash;
+  }, [[]]).reduce((hash, block) => {
+    return hash + block.reduce((result, num) => result ^ num, 0).toString(16)
+  }, '');
+}
+
+getResult(initialState, myInput); // Part 1.
+getKnotHash(initialState, myInput); // Part 2.
